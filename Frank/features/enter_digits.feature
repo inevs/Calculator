357a265b8f448f755cd19b89e7396caa090cf684
Sven Günther <sven.guenther@it-agile.de>
2012-11-07 15:26:08 +0100

    erster Frank-Test

---
+++ Frank/features/enter_digits.feature
@@ -0,0 +1,14 @@
+Feature: Enter digits
+  In order to calculate numbers
+  As a user
+  I want to enter digits
+
+Scenario: Enter Digits
+  Given I launch the app
+  When I enter "12.9"
+  Then I should see this in the display
+
+
+
+
+  
---
+++ Frank/features/step_definitions/enter_digits_steps.rb
@@ -0,0 +1,11 @@
+When /^I enter "(.*?)"$/ do |keys|
+  @input = keys;
+  keys.split("").each do |key|
+    touch("view marked:'#{key}'")
+  end
+end
+
+Then /^I should see this in the display$/ do
+  text = frankly_map("view marked:'displayLabel", "text").first
+  text.should == @input
+end
