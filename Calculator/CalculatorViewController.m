9ace2bf66ce12cdbf9b4658d8c128b962bd1a8b5
Sven Günther <sven.guenther@it-agile.de>
2012-11-07 15:09:18 +0100

    reset code and tests

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -1,52 +1,11 @@
 #import "CalculatorViewController.h"
 
-#define Period @"."
-
 @implementation CalculatorViewController
 
-- (void)viewDidLoad {
-	self.displayLabel.text = @"0";
-}
-
 - (IBAction)buttonTouched:(id)sender {
-	NSMutableString *input = [self inputFromDisplay];
-	NSString *character = [self characterFromButton:sender];
-	if ([self shouldAppendCharacter:character toPreviousInput:input]) {
-		[input appendString:character];
-	}
-	self.displayLabel.text = input;
 }
 
 - (IBAction)currencyButtonTouched:(id)sender {
-}
-
-- (BOOL)shouldAppendCharacter:(NSString *)character toPreviousInput:(NSString *)input {
-	BOOL result = YES;
-	if ([character isEqualToString:Period] && [input rangeOfString:Period].location != NSNotFound) {
-		result = NO;
-	}
-	return result;
-}
-
-- (NSString *)characterFromButton:(UIButton *)button {
-	NSString *character;
-	if ([button tag] == PeriodButton) {
-		character = Period;
-	} else {
-		character = [NSString stringWithFormat:@"%d", [button tag]];
-	}
-	return character;
-}
-
-- (NSMutableString *)inputFromDisplay {
-	NSMutableString *input;
-	NSString *previousInput = self.displayLabel.text;
-	if ([previousInput isEqualToString:@"0"]) {
-		input = [NSMutableString string];
-	} else {
-		input = [NSMutableString stringWithString:previousInput];
-	}
-	return input;
 }
 
 @end
--- CalculatorTests/CalculatorViewControllerTest.m
+++ CalculatorTests/CalculatorViewControllerTest.m
@@ -4,61 +4,7 @@
 @end
 
 @implementation CalculatorViewControllerTest {
-	CalculatorViewController *calculatorViewController;
-	UILabel *displayLabel;
 }
 
-- (void)setUp {
-	calculatorViewController = [[CalculatorViewController alloc] init];
-	displayLabel = [[UILabel alloc] init];
-	displayLabel.text = @"";
-	calculatorViewController.displayLabel = displayLabel;
-}
-
-- (UIButton *)buttonWithTag:(NSInteger)tag {
-	UIButton *button = [[UIButton alloc] init];
-	button.tag = tag;
-	return button;
-}
-
-- (void)testShowsEmptyDisplayOnStart {
-	[calculatorViewController viewDidLoad];
-	expect(displayLabel.text).to.equal(@"0");
-}
-
-- (void)testAppendsDigitsToInput {
-	[calculatorViewController buttonTouched:[self buttonWithTag:1]];
-	expect(displayLabel.text).to.equal(@"1");
-}
-
-- (void)testAppendsMultipleDigits {
-	[calculatorViewController buttonTouched:[self buttonWithTag:2]];
-	[calculatorViewController buttonTouched:[self buttonWithTag:3]];
-	expect(displayLabel.text).to.equal(@"23");
-}
-
-- (void)testAvoidsTrailingZero {
-	displayLabel.text = @"0";
-	[calculatorViewController buttonTouched:[self buttonWithTag:4]];
-	expect(displayLabel.text).to.equal(@"4");
-}
-
-- (void)testInsertsDecimalWithPeriod {
-	displayLabel.text = @"1";
-	[calculatorViewController buttonTouched:[self buttonWithTag:PeriodButton]];
-	expect(displayLabel.text).to.equal(@"1.");
-}
-
-- (void)testAllowsOnlyOnePeriod {
-	displayLabel.text = @"1.";
-	[calculatorViewController buttonTouched:[self buttonWithTag:PeriodButton]];
-	expect(displayLabel.text).to.equal(@"1.");
-}
-
-- (void)testAllowsOnlyOnePeriodInInput {
-	displayLabel.text = @"4.56";
-	[calculatorViewController buttonTouched:[self buttonWithTag:PeriodButton]];
-	expect(displayLabel.text).to.equal(@"4.56");
-}
 
 @end
